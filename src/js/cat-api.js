0c691c0a3f61ce5c0a61290b9e7f6552016dcebb
IlyRov <127045335+IlyRov@users.noreply.github.com>
2023-09-16 10:28:34 +0100

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -15,14 +15,14 @@ export async function fetchCatByBreed(breedId) {
     try {
         const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
         
-        if (Array.isArray(response.data) && response.data.length > 0) {
+        if (response.data && response.data.length > 0) {
             const catInfo = response.data[0];
     
             return {
-                breed: catInfo.breeds[0].name,
-                description: catInfo.breeds[0].description,
-                temperament: catInfo.breeds[0].temperament,
-                imageUrl: catInfo.url
+                breed: catInfo?.breeds?.[0]?.name || 'Інформація відсутня',
+                description: catInfo?.breeds?.[0]?.description || 'Інформація відсутня',
+                temperament: catInfo?.breeds?.[0]?.temperament || 'Інформація відсутня',
+                imageUrl: catInfo?.url || ''
             };
         } else {
             throw new Error("Кота з цією породою не знайдено.");
@@ -31,4 +31,5 @@ export async function fetchCatByBreed(breedId) {
         throw error;
     }
 }
+
 
