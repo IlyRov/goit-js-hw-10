ca6bd14ff36bf72163530a255f7eb3c12a591e8e
IlyRov <127045335+IlyRov@users.noreply.github.com>
2023-09-16 10:41:06 +0100

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,19 +1,19 @@
-
 import axios from "axios";
+
+const apiKey = 'live_YWg0u6gdp5wTKySikI3gzrI2JHrH2fKh4yR56BflBBj8nSSqZ2trrN0sPi0dzs46';
 
 export async function fetchBreeds() {
     try {
-        const response = await axios.get("https://api.thecatapi.com/v1/breeds");
+        const response = await axios.get(`https://api.thecatapi.com/v1/breeds?api_key=${apiKey}`);
         return response.data;
     } catch (error) {
         throw error;
     }
 }
 
-
 export async function fetchCatByBreed(breedId) {
     try {
-        const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
+        const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${apiKey}`);
         
         if (response.data && response.data.length > 0) {
             const catInfo = response.data[0];
